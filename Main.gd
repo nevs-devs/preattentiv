f35ed415b45dd4248d6fbb439000b9bbf306a672
Bluemi <bruno.schilling@protonmail.ch>
2020-04-28 17:32:11 +0200

    added rough cycle

--- Main.gd
+++ Main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const COUNTDOWN_TIME = 3
+
 const EXPLANATION_MODE = 0
 const COUNTDOWN_MODE = 1
 const SHOW_MODE = 2
@@ -7,10 +9,60 @@ const RESULT_MODE = 3
 
 var mode = EXPLANATION_MODE
 
-func _ready():
-	pass
+var time_counter = 0
+var duration = 1
 
-func show_explanation():
+func _ready():
+	start_explanation()
+
+func start_explanation():
+	print('start explanation')
+	$"Show".visible = true
 	$"Countdown".visible = false
 	$"Explanation".visible = true
-	$"Show".create("mode")
+	$"Result".visible = false
+	mode = EXPLANATION_MODE
+	# $"Show".show_objects("subject", ["distractor1", "distractor2"], count=10, show_subject=True)
+
+func start_countdown():
+	print('start countdown')
+	$"Countdown".visible = true
+	$"Explanation".visible = false
+	time_counter = COUNTDOWN_TIME
+	mode = COUNTDOWN_MODE
+
+func start_result():
+	print('result')
+	$"Show".visible = false
+	$"Countdown".visible = false
+	$"Explanation".visible = false
+	$"Result".visible = true
+	mode = RESULT_MODE
+
+func start_show():
+	print('start show')
+	$"Show".visible = true
+	$"Countdown".visible = false
+	$"Explanation".visible = false
+	$"Result".visible = false
+	mode = SHOW_MODE
+	time_counter = duration
+
+func _input(event):
+	if mode == EXPLANATION_MODE:
+		if event is InputEventKey and event.pressed and event.is_action("ui_accept"):
+			start_countdown()
+
+func _process(delta):
+	if mode == COUNTDOWN_MODE:
+		time_counter -= delta
+		if time_counter <= 0:
+			start_show()
+	if mode == SHOW_MODE:
+		time_counter -= delta
+		if time_counter <= 0:
+			start_result()
+
+func next_round():
+	print('next round')
+	start_explanation()
