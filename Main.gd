f3a5c33c36407b12729cafb73728645adae60969
Bluemi <bruno.schilling@protonmail.ch>
2020-04-28 16:55:54 +0200

    ...

--- Main.gd
+++ Main.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
 const EXPLANATION_MODE = 0
-const SHOW_MODE = 1
-const RESULT_MODE = 2
+const COUNTDOWN_MODE = 1
+const SHOW_MODE = 2
+const RESULT_MODE = 3
 
 var mode = EXPLANATION_MODE
+
+func _ready():
+	pass
+
+func show_explanation():
+	$"Countdown".visible = false
+	$"Explanation".visible = true
+	$"Show".create("mode")
