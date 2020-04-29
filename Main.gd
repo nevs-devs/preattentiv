07dad2c5afa85b27464f99815bbe523311151f9a
Bluemi <bruno.schilling@protonmail.ch>
2020-04-29 15:41:43 +0200

    ...

--- Main.gd
+++ Main.gd
@@ -38,8 +38,8 @@ const COUNTDOWN_MODE = 1
 const SHOW_MODE = 2
 const RESULT_MODE = 3
 
-# const DURATIONS = [4.0, 2.0, 1.5, 1.0, 0.75, 0.5, 0.375, 0.25, 0.125, 0.075]
-const DURATIONS = [0.375, 0.25]
+const DURATIONS = [4.0, 2.0, 1.5, 1.0, 0.75, 0.5, 0.375, 0.25, 0.125, 0.075]
+# const DURATIONS = [0.375, 0.25]
 var EXPERIMENTS = []
 
 var mode = EXPLANATION_MODE
